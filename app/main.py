05af883f18d91e639e9f554d48783065a7bd6fe7
Bohdan Mykyta <justkexitmail@gmail.com>
2025-03-28 01:56:19 +0200

    ^^

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            if friend["wearing_a_mask"] is False:
                 masks_to_buy += 1
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
